b92c64d6d3559f4915a67ae05607793b819cfdc4
DESKTOP-21HL8UE\Carlos <carlosrepillosa@gmail.com>
2020-09-01 22:17:46 -0400

    Mi primer commit

---
+++ src/main/java/com/platzi/mensajes_app/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.platzi.mensajes_app;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Main {
+    public static void main(String[] args){
+        Conexion conexion = new Conexion();
+        try{
+            Connection cnx = conexion.get_connection();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
